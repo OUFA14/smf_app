ff8397a626657d1c6c0c05825e5f42b09dffe816
OUFA14 <seifh0307@gmail.com>
2026-05-26 13:15:28 +0300

    Update announcements_page.dart

--- smf_app_last2/lib/Screens/announcements/announcements_page.dart
+++ smf_app_last2/lib/Screens/announcements/announcements_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import '../../services/announcements_service.dart';
 import '../../providers/language_provider.dart';
 import '../../theme/app_theme.dart';
 import 'announcement_model.dart';
